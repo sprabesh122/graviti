9586829ef6d25c8afb9191ff75133bc820111078
Prabesh Sah <sprabesh122@gmail.com>
2023-01-30 23:48:32 +0530

    Fixed Frontend Works

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import {
   HStack,
   // IconButton,
   Input,
+  InputGroup,
+  InputLeftElement,
   SkeletonText,
   Text,
 } from "@chakra-ui/react";
 // import { FaLocationArrow, FaTimes } from "react-icons/fa";
+
+import SearchIcon from "@chakra-ui/icon";
 
 import {
   useJsApiLoader,
@@ -35,7 +39,7 @@ function App() {
   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const [directionsResponse, setDirectionsResponse] = useState(null);
   const [distance, setDistance] = useState("");
-  // const [duration, setDuration] = useState("");
+  const [duration, setDuration] = useState("");
 
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef();
@@ -60,7 +64,7 @@ function App() {
     });
     setDirectionsResponse(results);
     setDistance(results.routes[0].legs[0].distance.text);
-    // setDuration(results.routes[0].legs[0].duration.text);
+    setDuration(results.routes[0].legs[0].duration.text);
   }
 
   /*
@@ -75,122 +79,159 @@ function App() {
 
   return (
     <>
-      <Opening>
-        <Navbar />
-      </Opening>
+      <div>
+        <div>
+          <Navbar />
+        </div>
 
-      <Flex
-        position="fixed"
-        flexDirection="row"
-        alignItems="flex-start"
-        h="100vh"
-        w="100vw"
-        // margin="100px"
-        mt="82px"
-        bgColor="#f4f8fa"
-      >
-        <Box
-          p={4}
-          borderRadius="lg"
-          // m={"15%"}
-          mt={"4%"}
-          ml="80px"
-          // bgColor="#f4f8fa"
-          // shadow="base"
-          W="50%"
-          height="100%"
-          zIndex="modal"
-          display="inline"
-          fontWeight={"bold"}
-        >
-          <HStack justify={"space-around"} flexDirection="column">
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
+        <div>
+          <div
+            style={{
+              backgroundColor: "#f4f8fa",
+              height: "60px",
+              color: "#1B31A8",
+              textAlign: "center",
+              width: "100vw",
+              fontSize: "20px",
+              fontWeight: "400",
+            }}
+          >
+            Let's calculate <b>distance</b> from Google maps
+          </div>
+
+          <div>
+            <Flex
+              position="fixed"
+              flexDirection="row"
+              alignItems="flex-start"
+              h="100vh"
+              w="100vw"
+              // margin="100px"
+              // mt="82px"
+              bgColor="#f4f8fa"
             >
-              <div>
-                <Box width="250px" height="15vh">
-                  <Text>Origin:</Text>
-                  <Autocomplete>
-                    <Input
-                      type="text"
-                      placeholder="Origin"
-                      ref={originRef}
-                      bgColor="#fff"
-                    />
-                  </Autocomplete>
-                </Box>
-
-                <Box width="250px" height="15vh">
-                  <Text>Stop:</Text>
-                  <Autocomplete>
-                    <Input
-                      type="text"
-                      placeholder="Current Stop"
-                      ref={destiantionRef}
-                      bgColor="#fff"
-                    />
-                  </Autocomplete>
-                </Box>
-
-                <Box width="250px" height="15vh">
-                  <Text>Destination:</Text>
-                  <Autocomplete>
-                    <Input
-                      type="text"
-                      placeholder="Destination"
-                      ref={destiantionRef}
-                      bgColor="#fff"
-                    />
-                  </Autocomplete>
-                </Box>
-              </div>
-
-              <div style={{ marginTop: "110px", paddingLeft: "120px" }}>
-                <ButtonGroup>
-                  <Button
-                    color="#ffffff"
-                    backgroundColor={"#1B31A8"}
-                    type="submit"
-                    onClick={calculateRoute}
-                    borderRadius="32px"
-                    width={"141px"}
-                    height={"62px"}
-                    top={"-30px"}
-                    gap={"10px"}
+              <Box
+                p={4}
+                borderRadius="lg"
+                // m={"15%"}
+                mt={"4%"}
+                ml="80px"
+                // bgColor="#f4f8fa"
+                // shadow="base"
+                W="50%"
+                height="100%"
+                zIndex="modal"
+                display="inline"
+                fontWeight={"bold"}
+              >
+                <HStack justify={"space-around"} flexDirection="column">
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
                   >
-                    Calculate
-                  </Button>
+                    <div>
+                      <Box width="250px" height="15vh">
+                        <Text>Origin:</Text>
+                        <Autocomplete>
+                          <InputGroup>
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<SearchIcon color="black" />}
+                            />
 
-                  {/* Button to clear the input field  */}
-                  {/* <IconButton
+                            <Input
+                              type="text"
+                              placeholder="Origin"
+                              ref={originRef}
+                              bgColor="#fff"
+                            />
+                          </InputGroup>
+                        </Autocomplete>
+                      </Box>
+
+                      <Box width="250px" height="15vh">
+                        <Text>Stop:</Text>
+                        <Autocomplete>
+                          <InputGroup>
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<SearchIcon color="black" />}
+                            />
+
+                            <Input
+                              type="text"
+                              placeholder="Current Stop"
+                              ref={destiantionRef}
+                              bgColor="#fff"
+                            />
+                          </InputGroup>
+                        </Autocomplete>
+                      </Box>
+
+                      <Box width="250px" height="15vh">
+                        <Text>Destination:</Text>
+                        <Autocomplete>
+                          <InputGroup>
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<SearchIcon color="black" />}
+                            />
+
+                            <Input
+                              type="text"
+                              placeholder="Destination"
+                              ref={destiantionRef}
+                              bgColor="#fff"
+                            />
+                          </InputGroup>
+                        </Autocomplete>
+                      </Box>
+                    </div>
+
+                    <div style={{ marginTop: "110px", paddingLeft: "120px" }}>
+                      <ButtonGroup>
+                        <Button
+                          color="#ffffff"
+                          backgroundColor={"#1B31A8"}
+                          type="submit"
+                          onClick={calculateRoute}
+                          borderRadius="32px"
+                          width={"141px"}
+                          height={"62px"}
+                          top={"-30px"}
+                          gap={"10px"}
+                        >
+                          Calculate
+                        </Button>
+
+                        {/* Button to clear the input field  */}
+                        {/* <IconButton
                     aria-label="center back"
                     icon={<FaTimes />}
                     onClick={clearRoute}
                   /> */}
-                </ButtonGroup>
-              </div>
-            </div>
-          </HStack>
+                      </ButtonGroup>
+                    </div>
+                  </div>
+                </HStack>
 
-          <div style={{ backgroundColor: "#ffffff", height: "120px" }}>
-            <div style={{ height: "50%", alignItems: "center" }}>
-              <HStack spacing={4} mt={4} justifyContent="space-between">
-                <Text justify="space-between" margin="5px">
-                  Distance: <span style={{ color: "#0079FF" }}>{distance}</span>
-                </Text>
-                {/* Duration  */}
-                {/* 
+                <div style={{ backgroundColor: "#ffffff", height: "120px" }}>
+                  <div style={{ height: "50%", alignItems: "center" }}>
+                    <HStack spacing={4} mt={4} justifyContent="space-between">
+                      <Text justify="space-between" margin="5px">
+                        Distance:{" "}
+                        <span style={{ color: "#0079FF" }}>{distance}</span>
+                      </Text>
+                      {/* Duration  */}
+                      {/* 
                 <Text>
                   Duration: <span style={{ color: "#0079FF" }}>{duration}</span>{" "}
-                </Text>
-                 */}
-                {/* 
+                </Text> */}
 
-                {/* Relocation arrow  
+                      {/* Relocation arrow  
                 <IconButton
                   aria-label="center back"
                   icon={<FaLocationArrow />}
@@ -201,51 +242,54 @@ function App() {
                   }}
                 />
                 */}
-              </HStack>
-            </div>
+                    </HStack>
+                  </div>
 
-            <div
-              style={{
-                backgroundColor: "#f4f8fa",
-                height: "50%",
-                padding: "5px",
-              }}
-            >
-              The distance via the seleted route is {distance} kms.
-            </div>
+                  <div
+                    style={{
+                      backgroundColor: "#f4f8fa",
+                      height: "50%",
+                      padding: "5px",
+                    }}
+                  >
+                    The distance via the seleted route is {distance} kms.
+                  </div>
+                </div>
+              </Box>
+
+              <Box
+                h="60%"
+                w="50%"
+                mt="6%"
+                mb="5"
+                ml="10%"
+                mr="5%"
+                padding="4"
+                justifyContent="space-between"
+              >
+                {/* Google Map Box */}
+                <GoogleMap
+                  center={center}
+                  zoom={15}
+                  mapContainerStyle={{ width: "100%", height: "100%" }}
+                  options={{
+                    zoomControl: false,
+                    streetViewControl: false,
+                    mapTypeControl: false,
+                    fullscreenControl: false,
+                  }}
+                  onLoad={(map) => setMap(map)}
+                >
+                  <Marker position={center} />
+                  {directionsResponse && (
+                    <DirectionsRenderer directions={directionsResponse} />
+                  )}
+                </GoogleMap>
+              </Box>
+            </Flex>
           </div>
-        </Box>
-
-        <Box
-          h="60%"
-          w="50%"
-          mt="6%"
-          mb="5"
-          ml="10%"
-          mr="5%"
-          padding="4"
-          justifyContent="space-between"
-        >
-          {/* Google Map Box */}
-          <GoogleMap
-            center={center}
-            zoom={15}
-            mapContainerStyle={{ width: "100%", height: "100%" }}
-            options={{
-              zoomControl: false,
-              streetViewControl: false,
-              mapTypeControl: false,
-              fullscreenControl: false,
-            }}
-            onLoad={(map) => setMap(map)}
-          >
-            <Marker position={center} />
-            {directionsResponse && (
-              <DirectionsRenderer directions={directionsResponse} />
-            )}
-          </GoogleMap>
-        </Box>
-      </Flex>
+        </div>
+      </div>
     </>
   );
 }
